39f33069d15f99c9f08e65b995b0564399f4d668
kafiabdullah-me <contact.kafiabdullah@gmail.com>
2026-09-23 22:56:57 +0600

    Add files via upload

--- admin/firebase-config.js
+++ admin/firebase-config.js
@@ -1,9 +1,9 @@
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyCuceRc2cKcZCSAXJnq1i-dGxBazcMHy-8",
   authDomain: "kafi-abdullah-portfolio.firebaseapp.com",
   projectId: "kafi-abdullah-portfolio",
   storageBucket: "kafi-abdullah-portfolio.firebasestorage.app",
   messagingSenderId: "888081919366",
-  appId: "1:888081919366:web:a0edf5b778e5d7334a1007",
-  measurementId: "G-PHRH3HYWJT"
+  appId: "1:888081919366:web:6cd9911a9cb8c21d4a1007",
+  measurementId: "G-DXTQ5J072L"
 };
